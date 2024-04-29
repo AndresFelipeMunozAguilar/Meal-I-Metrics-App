87858f847b1faae358f68d521b61806f6025b307
Juand2602 <juand_sepulveda@hotmail.com>
2024-04-29 12:03:18 -0500

    agg estilo

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:mealimetrics/widgets/home_widget.dart';
 import 'package:mealimetrics/widgets/actualizar_datos.dart';
 
-
 const supabaseUrl = 'https://ddyveuettsjaxmdbijgb.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkeXZldWV0dHNqYXhtZGJpamdiIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMzA1MDgzNCwiZXhwIjoyMDI4NjI2ODM0fQ.FoRjsJj9d7R-XSkNN4hokmfmTG-mEcr2QuWWT9RFnxc';
 
@@ -41,7 +40,7 @@ class MyApp extends StatelessWidget {
     onBackground: Colors.black, // Color del texto sobre el fondo
     onError: Colors.white, // Color del texto sobre el color de error (opcional)
     brightness: Brightness.light, // Brillo (puede ser light o dark)
-  );
+  ); 
 
   // This widget is the root of your application.
   @override
